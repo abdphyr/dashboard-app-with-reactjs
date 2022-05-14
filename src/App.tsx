4a30595f8d925356b59afb6eec16b4e3347a8ae7
Abdumannon <abdphyr@gmail.com>
2022-05-14 12:00:08 +0500

    simple-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './app.css';
+import Navbar from './components/Navbar';
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className='app'>
+        <Routes>
+          <Route path='/one' element={<div>First Route</div>} />
+          <Route path='/two' element={<div>Second Route</div>} />
+          <Route path='/three' element={<div>Third Route</div>} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
